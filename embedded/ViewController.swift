67a4081db7e8a46c19a97fca628e9e1ffec333f4
Pavel Ivanov <ast0r89@gmail.com>
2019-12-01 23:12:19 +0200

    Initial Commit

---
+++ embedded/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  embedded
+//
+//  Created by Pavel Ivanov on 12/1/19.
+//  Copyright © 2019 Pavel Ivanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
